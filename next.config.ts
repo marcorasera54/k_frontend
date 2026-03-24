2872023241b52835c13128381805681b10165af9
marcorasera54 <laurapausini535@gmail.com>
2026-03-23 17:14:16 -0700

    implemented a fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://136.114.25.155/api/:path*",
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
