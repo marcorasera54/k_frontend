3a855ede695fdd8b21191c62c9b3d6e3d1ae06a0
marcorasera54 <laurapausini535@gmail.com>
2026-03-23 17:34:05 -0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination:
+          "https://capabilities-programmer-retrieval-lyric.trycloudflare.com/api/v1/:path*",
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
